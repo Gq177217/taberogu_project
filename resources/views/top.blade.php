ebac75ad288eaa3d9b97408dd5a6ac76196a8691
Yasuko kawashima <yasuko.kawashima@gmail.com>
2023-10-07 09:47:37 +0900

    seachbo修正

--- resources/views/top.blade.php
+++ resources/views/top.blade.php
@@ -22,429 +22,106 @@
             margin: 0
         }
 
-        a {
-            background-color: transparent
-        }
-
-        [hidden] {
-            display: none
-        }
-
-        html {
-            font-family: system-ui, -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Helvetica Neue, Arial, Noto Sans, sans-serif, Apple Color Emoji, Segoe UI Emoji, Segoe UI Symbol, Noto Color Emoji;
-            line-height: 1.5
-        }
-
-        *,
-        :after,
-        :before {
-            box-sizing: border-box;
-            border: 0 solid #e2e8f0
-        }
-
-        a {
-            color: inherit;
-            text-decoration: inherit
-        }
-
-        svg,
-        video {
-            display: block;
-            vertical-align: middle
-        }
-
-        video {
-            max-width: 100%;
-            height: auto
-        }
-
-        .bg-white {
-            --tw-bg-opacity: 1;
-            background-color: rgb(255 255 255 / var(--tw-bg-opacity))
-        }
-
-        .bg-gray-100 {
-            --tw-bg-opacity: 1;
-            background-color: rgb(243 244 246 / var(--tw-bg-opacity))
-        }
-
-        .border-gray-200 {
-            --tw-border-opacity: 1;
-            border-color: rgb(229 231 235 / var(--tw-border-opacity))
-        }
-
-        .border-t {
-            border-top-width: 1px
-        }
-
-        .flex {
-            display: flex
-        }
-
-        .grid {
-            display: grid
-        }
-
-        .hidden {
-            display: none
-        }
-
-        .items-center {
-            align-items: center
-        }
-
-        .justify-center {
-            justify-content: center
-        }
-
-        .font-semibold {
-            font-weight: 600
-        }
-
-        .h-5 {
-            height: 1.25rem
-        }
-
-        .h-8 {
-            height: 2rem
-        }
-
-        .h-16 {
-            height: 4rem
-        }
-
-        .text-sm {
-            font-size: .875rem
-        }
-
-        .text-lg {
-            font-size: 1.125rem
-        }
-
-        .leading-7 {
-            line-height: 1.75rem
-        }
-
-        .mx-auto {
-            margin-left: auto;
-            margin-right: auto
-        }
-
-        .ml-1 {
-            margin-left: .25rem
-        }
-
-        .mt-2 {
-            margin-top: .5rem
-        }
-
-        .mr-2 {
-            margin-right: .5rem
-        }
-
-        .ml-2 {
-            margin-left: .5rem
-        }
-
-        .mt-4 {
-            margin-top: 1rem
-        }
-
-        .ml-4 {
-            margin-left: 1rem
-        }
-
-        .mt-8 {
-            margin-top: 2rem
-        }
-
-        .ml-12 {
-            margin-left: 3rem
-        }
-
-        .-mt-px {
-            margin-top: -1px
-        }
-
-        .max-w-6xl {
-            max-width: 72rem
-        }
-
-        .min-h-screen {
-            min-height: 100vh
-        }
-
-        .overflow-hidden {
-            overflow: hidden
-        }
-
-        .p-6 {
-            padding: 1.5rem
-        }
-
-        .py-4 {
-            padding-top: 1rem;
-            padding-bottom: 1rem
-        }
-
-        .px-6 {
-            padding-left: 1.5rem;
-            padding-right: 1.5rem
-        }
-
-        .pt-8 {
-            padding-top: 2rem
-        }
-
-        .fixed {
-            position: fixed
-        }
-
-        .relative {
-            position: relative
-        }
-
-        .top-0 {
-            top: 0
-        }
-
-        .right-0 {
-            right: 0
-        }
-
-        .shadow {
-            --tw-shadow: 0 1px 3px 0 rgb(0 0 0 / .1), 0 1px 2px -1px rgb(0 0 0 / .1);
-            --tw-shadow-colored: 0 1px 3px 0 var(--tw-shadow-color), 0 1px 2px -1px var(--tw-shadow-color);
-            box-shadow: var(--tw-ring-offset-shadow, 0 0 #0000), var(--tw-ring-shadow, 0 0 #0000), var(--tw-shadow)
-        }
-
-        .text-center {
-            text-align: center
-        }
-
-        .text-gray-200 {
-            --tw-text-opacity: 1;
-            color: rgb(229 231 235 / var(--tw-text-opacity))
-        }
-
-        .text-gray-300 {
-            --tw-text-opacity: 1;
-            color: rgb(209 213 219 / var(--tw-text-opacity))
-        }
-
-        .text-gray-400 {
-            --tw-text-opacity: 1;
-            color: rgb(156 163 175 / var(--tw-text-opacity))
-        }
-
-        .text-gray-500 {
-            --tw-text-opacity: 1;
-            color: rgb(107 114 128 / var(--tw-text-opacity))
-        }
-
-        .text-gray-600 {
-            --tw-text-opacity: 1;
-            color: rgb(75 85 99 / var(--tw-text-opacity))
-        }
-
-        .text-gray-700 {
-            --tw-text-opacity: 1;
-            color: rgb(55 65 81 / var(--tw-text-opacity))
-        }
-
-        .text-gray-900 {
-            --tw-text-opacity: 1;
-            color: rgb(17 24 39 / var(--tw-text-opacity))
-        }
-
-        .underline {
-            text-decoration: underline
-        }
-
-        .antialiased {
-            -webkit-font-smoothing: antialiased;
-            -moz-osx-font-smoothing: grayscale
-        }
-
-        .w-5 {
-            width: 1.25rem
-        }
-
-        .w-8 {
-            width: 2rem
-        }
-
-        .w-auto {
-            width: auto
-        }
-
-        .grid-cols-1 {
-            grid-template-columns: repeat(1, minmax(0, 1fr))
-        }
-
-        @media (min-width:640px) {
-            .sm\:rounded-lg {
-                border-radius: .5rem
-            }
-
-            .sm\:block {
-                display: block
-            }
-
-            .sm\:items-center {
-                align-items: center
-            }
-
-            .sm\:justify-start {
-                justify-content: flex-start
-            }
-
-            .sm\:justify-between {
-                justify-content: space-between
-            }
-
-            .sm\:h-20 {
-                height: 5rem
-            }
-
-            .sm\:ml-0 {
-                margin-left: 0
-            }
-
-            .sm\:px-6 {
-                padding-left: 1.5rem;
-                padding-right: 1.5rem
-            }
-
-            .sm\:pt-0 {
-                padding-top: 0
-            }
-
-            .sm\:text-left {
-                text-align: left
-            }
-
-            .sm\:text-right {
-                text-align: right
-            }
-        }
-
-        @media (min-width:768px) {
-            .md\:border-t-0 {
-                border-top-width: 0
-            }
-
-            .md\:border-l {
-                border-left-width: 1px
-            }
-
-            .md\:grid-cols-2 {
-                grid-template-columns: repeat(2, minmax(0, 1fr))
-            }
-        }
-
-        @media (min-width:1024px) {
-            .lg\:px-8 {
-                padding-left: 2rem;
-                padding-right: 2rem
-            }
-        }
-
-        @media (prefers-color-scheme:dark) {
-            .dark\:bg-gray-800 {
-                --tw-bg-opacity: 1;
-                background-color: rgb(31 41 55 / var(--tw-bg-opacity))
-            }
-
-            .dark\:bg-gray-900 {
-                --tw-bg-opacity: 1;
-                background-color: rgb(17 24 39 / var(--tw-bg-opacity))
-            }
-
-            .dark\:border-gray-700 {
-                --tw-border-opacity: 1;
-                border-color: rgb(55 65 81 / var(--tw-border-opacity))
-            }
-
-            .dark\:text-white {
-                --tw-text-opacity: 1;
-                color: rgb(255 255 255 / var(--tw-text-opacity))
-            }
-
-            .dark\:text-gray-400 {
-                --tw-text-opacity: 1;
-                color: rgb(156 163 175 / var(--tw-text-opacity))
-            }
-
-            .dark\:text-gray-500 {
-                --tw-text-opacity: 1;
-                color: rgb(107 114 128 / var(--tw-text-opacity))
-            }
-        }
-    </style>
-
-    <style>
         body {
             font-family: 'Nunito', sans-serif;
         }
     </style>
+    <style>
+        .shopSearch {
+            /*display: block;*/
+            /*width: 300px;
+                                                                        height: 0px;
+                                                                        padding: 5px;*/
+            font-size: 1rem;
+            font-weight: 400;
+            line-height: 1.6;
+            /*color: rgba(186, 186, 186, 0.845);*/
+            /*background-color: rgba(255, 0, 0, 0.882);*/
+            background-clip: padding-box;
+            /*border: 1px sold gray;*/
+            appearance: none;
+            border-radius: 0.375rem;
+            tansition: border-color .15s;
+            box-sizing: content-box;
+            /*margin: 0 auto;*/
+            jdisplay: flex;
+            justify-content: center;
+        }
 
-    <link rel="stylesheet" href="{{ asset('css/store.css') }}">
+        .container {
+            display: flex;
+            align-items: center;
+            margin-top: 30px;
+        }
+
+        .category {
+            display: block;
+            width: 230px;
+            padding: 0.375rem 2.25rem 0.375rem 0.75rem;
+            font-size: 1rem;
+            font-weight: 400;
+            line-height: 1.6;
+            color: black;
+            box-sizing: border-box;
+            margin: 0 auto;
+            display: flex;
+            justify-content: center;
+        }
+
+        body {
+            background-image: url(img/NYC.jpeg);
+            /* 画像を常に上下左右の中央に配置させる */
+            background-position: center center;
+            /* 画像を繰り返し表示しない */
+            background-repeat: no-repeat;
+            /* ページなどのコンテンツの高さが画像の高さより大きい時動に固定する */
+            background-attachment: fixed;
+            /* 画面、ブラウザのサイズに基づいて、背景画像を調整 */
+            background-size: cover;
+            /* 背景画像が表示されるまでの間に表示される(待機中に表示される)背景のカラー */
+            background-color: #000000;
+
+        }
+    </style>
+
 </head>
 
 <body>
     @extends('layouts.app')
     @section('content')
-        <div class="d-flex justify-content-center align-items-center top-wrapper">
-            <div class="d-flex justify-content-center">
-                <div class="search-area px-5 py-4">
-                    <div class="search">
-                        <h1 class="mt-2">おいしいお店を探す</h1>
-                        <div class="d-flex flex-wrap flex-sm-nowrap">
-                            <form action="{{ route('stores') }}">
-                                <div class="me-2 mb-2">
-                                    <input name="keyword" type="text"/ placeholder="店名">
-                                    <select class="form-control form-select" name="category" id="">
-                                        <option selected>Category</option>
-                                        @foreach ($categories as $category)
-                                            <option value="{{ $category->id }}">{{ $category->major_category_name }}
-                                            </option>
-                                        @endforeach
-                                    </select>
+        <!-- <div class="d-flex justify-content-center align-items-center top-wrapper">
+                                                                                        <div class="d-flex justify-content-center">-->
+        <div class="search-area px-5 py-4">
+            <div class="search">
+                <h1 class="mt-2">おいしいお店を探す</h1>
+                <!--<div class="d-flex flex-wrap flex-sm-nowrap">-->
+                <form action="{{ route('stores') }}">
+                    <!-- <div class="me-2 mb-2">-->
+                    <div class="container">
+                        <div class="item">
 
-                                    <input type="submit" value="検索">
-                            </form>
+                        </div>
+                        <div class="item">
+                            <div class="shopSearch"><input name="keyword" type="text"/ placeholder="店名"></div>
+                        </div>
+                        <div class="item">
+                            <div class="category">
+                                <select class="form-control form-select" name="category" id="">
+                                    <option selected>Category</option>
+                                    @foreach ($categories as $category)
+                                        <option value="{{ $category->id }}">{{ $category->major_category_name }} </option>
+                                    @endforeach
+                                </select>
+                            </div>
+                        </div>
+                        <div class="item">
+                            <input type="submit" value="検索">
                         </div>
                     </div>
-                @endsection
-                <style>
-                    body {
-                        background-image: url(img/NYC.jpeg);
-                        /* 画像を常に上下左右の中央に配置させる */
-                        background-position: center center;
-                        /* 画像を繰り返し表示しない */
-                        background-repeat: no-repeat;
-                        /* ページなどのコンテンツの高さが画像の高さより大きい時動に固定する */
-                        background-attachment: fixed;
-                        /* 画面、ブラウザのサイズに基づいて、背景画像を調整 */
-                        background-size: cover;
-                        /* 背景画像が表示されるまでの間に表示される(待機中に表示される)背景のカラー */
-                        background-color: #000000;
-
-                    }
-                </style>
+                </form>
             </div>
         </div>
-    </div>
-    </div>
-
-    <!--<style>
-        body {
-            background-image: url(../public/img/NYC.jpeg);
-            background-repeat: no-repeat
-        }
-    </style>-->
-
+    @endsection
 </body>
 
 </html>
